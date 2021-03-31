20eb4288ae42df8228bdc691d5bb77e91a9ca31e
AhMing <keeshengming@outlook.com>
2021-03-31 17:43:57 +0800

    Remove residual relationship and fix the discount class to variation discount

--- app/Models/Variation.php
+++ app/Models/Variation.php
@@ -14,19 +14,9 @@ class Variation extends Model
         return $this->belongsTo(Item::class);
     }
 
-    public function parent(): \Illuminate\Database\Eloquent\Relations\HasOne
-    {
-        return $this->hasOne(Variation::class, "parent");
-    }
-
-    public function children(): \Illuminate\Database\Eloquent\Relations\HasMany
-    {
-        return $this->hasMany(Variation::class, "child");
-    }
-
     public function discounts(): \Illuminate\Database\Eloquent\Relations\HasMany
     {
-        return $this->hasMany(WholesaleDiscount::class);
+        return $this->hasMany(VariationDiscount::class);
     }
 
     public function inventories(): \Illuminate\Database\Eloquent\Relations\HasMany

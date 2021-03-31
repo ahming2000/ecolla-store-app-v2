cadabb55c215575ccf8a446511350e32811ac99c
AhMing <keeshengming@outlook.com>
2021-03-31 17:47:44 +0800

    Add variable compact function on item controller

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -2,19 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\Item;
+use App\Models\ItemUtil;
 use Illuminate\Http\Request;
 
 class ItemsController extends Controller
 {
     public function index(){
-        return view('item.index');
+
+        $items = Item::join('item_utils', 'item_utils.item_id', '=', 'items.id')
+            ->where('is_listed', '=', 1)->get();
+        $categories = Category::all();
+
+        return view($this->getLang() . '.item.index', compact('items', 'categories'));
     }
 
 
     public function view(){
-        return view('item.view');
+        return view($this->getLang() . '.item.view');
     }
 
-
+    private function getLang(){
+        return substr($_SERVER['REQUEST_URI'], 1, 2);
+    }
 
 }

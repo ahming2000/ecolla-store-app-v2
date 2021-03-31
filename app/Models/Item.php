e17c4d20accd18312126496759b36be3cb68cc84
AhMing <keeshengming@outlook.com>
2021-03-31 17:45:35 +0800

    Add foreign key identifier and add getCoverImage, getListedCount and getPriceRange function.

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -9,14 +9,50 @@ class Item extends Model
 {
     use HasFactory;
 
+    public static function getListedCount(): int
+    {
+        return Item::join('item_utils', 'item_utils.item_id', '=', 'items.id')->where('is_listed', '=', 1)->count();
+    }
+
+    public function getCoverImage(): string
+    {
+        $value = "";
+        foreach($this->images as $i){
+            $value = $i['image'];
+            break;
+        }
+        return $value;
+    }
+
+    public function getPriceRange(): array
+    {
+        $max = 0.0;
+        $min = 0.0;
+        foreach($this->variations as $v){
+            $price = $v['price'];
+            // TODO - check the discount price also
+
+            if($price > $max){
+                $max = $price;
+            }
+
+            if($price < $min or $min == 0.0){
+                $min = $price;
+            }
+        }
+        $max = number_format($max, 2);
+        $min = number_format($min, 2);
+        return ['max' => $max, 'min' => $min];
+    }
+
     public function variations(): \Illuminate\Database\Eloquent\Relations\HasMany
     {
-        return $this->hasMany(Variation::class);
+        return $this->hasMany(Variation::class, 'item_id', 'id');
     }
 
     public function images(): \Illuminate\Database\Eloquent\Relations\HasMany
     {
-        return $this->hasMany(ItemImage::class);
+        return $this->hasMany(ItemImage::class, 'item_id', 'id');
     }
 
     public function util(): \Illuminate\Database\Eloquent\Relations\HasOne
@@ -26,7 +62,7 @@ class Item extends Model
 
     public function categories(): \Illuminate\Database\Eloquent\Relations\HasMany
     {
-        return $this->hasMany(Classification::class);
+        return $this->hasMany(Item::class);
     }
 
     public function userRating(): \Illuminate\Database\Eloquent\Relations\HasMany

934459ba13fd8d5f18eab167c62a9a16fb37ad2d
AhMing <keeshengming@outlook.com>
2021-04-03 17:17:27 +0800

    Remove extra word

--- resources/views/ch/item/index.blade.php
+++ resources/views/ch/item/index.blade.php
@@ -71,7 +71,7 @@
 
                             <div class="row">
                                 <div class="col text-left">
-                                    <i class="icofont-cart-alt"></i> 已售出 {{ $item->util->sold }} 个
+                                    <i class="icofont-cart-alt"></i> 已售出 {{ $item->util->sold }}
                                 </div>
                                 <div class="col text-right">
                                     <i class="icofont-eye"></i> {{ $item->util->view_count }}

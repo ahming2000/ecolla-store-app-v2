7d23388c0d7ee20b5883b80199111f189f0196e6
AhMing <keeshengming@outlook.com>
2021-04-03 17:11:27 +0800

    Remove shipping fee and add can check out flag, order verify id for pick up, customer for delivery. Most of the function now using barcode instead of variation object to verify. getShippingFee, canCheckOut, changeOrderMode, updateCustomerData function added

--- app/Session/Cart.php
+++ app/Session/Cart.php
@@ -4,6 +4,7 @@
 namespace App\Session;
 
 
+use App\Models\Customer;
 use App\Models\Variation;
 
 /**
@@ -16,8 +17,10 @@ class Cart
     public static string $DEFAULT_SESSION_NAME = 'ecollaCart';
 
     public array $cartItems;
-    public float $shippingFee;
     public string $orderMode;
+    public bool $canCheckOut;
+    public string $orderVerifyId;
+    public Customer $customer;
 
     public function start(){
 
@@ -25,22 +28,28 @@ class Cart
             $this->pullSessionCart(session(Cart::$DEFAULT_SESSION_NAME));
         } else{
             $this->cartItems = array();
-            $this->shippingFee = 0.0;
             $this->orderMode = Cart::$DEFAULT_ORDER_MODE;
+            $this->canCheckOut = false;
+            $this->orderVerifyId = "";
+            $this->customer = new Customer();
             $this->pushSessionCart();
         }
     }
 
     /**
      * @param array $cartItems
-     * @param float $shippingFee
      * @param string $orderMode
+     * @param bool $canCheckOut
+     * @param string $orderVerifyId
+     * @param Customer $customer
      */
-    public function importCart(array $cartItems, float $shippingFee, string $orderMode)
+    public function importCart(array $cartItems, string $orderMode, bool $canCheckOut, string $orderVerifyId, Customer $customer)
     {
         $this->cartItems = $cartItems;
-        $this->shippingFee = $shippingFee;
         $this->orderMode = $orderMode;
+        $this->canCheckOut = $canCheckOut;
+        $this->orderVerifyId = $orderVerifyId;
+        $this->customer = $customer;
     }
 
     /**
@@ -66,11 +75,11 @@ class Cart
     }
 
     /**
-     * @param Variation $variation
+     * @param $barcode
      */
-    public function deleteItem(Variation $variation){
+    public function deleteItem($barcode){
         for($i = 0; $i < sizeof($this->cartItems); $i++){
-            if($this->cartItems[$i]->variation === $variation){
+            if($this->cartItems[$i]->variation->barcode === $barcode){
                 unset($this->cartItems[$i]);
                 break;
             }
@@ -86,12 +95,13 @@ class Cart
     }
 
     /**
-     * @param $variation
+     * @param $barcode
      * @param $quantity
      */
-    public function editQuantity($variation, $quantity){
+    public function editQuantity($barcode, $quantity){
+
         foreach ($this->cartItems as $cartItem){
-            if($cartItem->variation === $variation){
+            if($cartItem->variation->barcode === $barcode && $cartItem->quantity + $quantity >= 1){
                 $cartItem->quantity += $quantity;
             }
         }
@@ -99,13 +109,46 @@ class Cart
         $this->pushSessionCart();
     }
 
-    /**
-     *
-     */
     public function resetCart(){
-        session()->pull(Cart::$DEFAULT_SESSION_NAME);
         unset($this->cartItems);
         $this->cartItems = array();
+        $this->canCheckOut = false;
+        $this->pushSessionCart();
+    }
+
+    public function changeOrderMode(string $mode){
+        $this->orderMode = $mode;
+        $this->pushSessionCart();
+    }
+
+    public function updateCustomerData($customerData){
+        foreach ($customerData as $key => $value){
+            $this->customer->setAttribute($key, $value);
+        }
+
+        $this->pushSessionCart();
+    }
+
+    public function updateOrderVerifyId($orderVerifyId){
+        $this->orderVerifyId = $orderVerifyId['order_verify_id'];
+        $this->pushSessionCart();
+    }
+
+    private function canCheckOut(){
+        if($this->orderMode == 'delivery'){
+            if(strtolower($this->customer->area) == 'kampar' && !empty($this->cartItems)){
+                $this->canCheckOut = true;
+            } else {
+                $this->canCheckOut = false;
+            }
+        } else{
+            if($this->orderVerifyId != "" && !empty($this->cartItems)){
+                $this->canCheckOut = true;
+            } else{
+                $this->canCheckOut = false;
+            }
+        }
+
     }
 
     /**
@@ -124,26 +167,41 @@ class Cart
 
     private function pullSessionCart(Cart $cart){
         $this->cartItems = $cart->cartItems;
-        $this->shippingFee = $cart->shippingFee;
         $this->orderMode = $cart->orderMode;
+        $this->canCheckOut = $cart->canCheckOut;
+        $this->orderVerifyId = $cart->orderVerifyId;
+        $this->customer = $cart->customer;
     }
 
     private function pushSessionCart(){
+        $this->canCheckOut();
         session([Cart::$DEFAULT_SESSION_NAME => $this]);
     }
 
     public function getCartCount(): int
     {
-        return sizeof($this->cartItems);
+        return sizeof($this->cartItems) ?? 0;
     }
 
     public function getSubTotal(): float
     {
         $total = 0.0;
         foreach($this->cartItems as $cartItem){
-            $total += $cartItem->variation * $cartItem->quantity;
+            $total += $cartItem->getSubPrice();
         }
         return $total;
+    }
+
+    public function getShippingFee(): float
+    {
+        $fee = 0.0;
+
+        if(strtolower($this->customer->area) == 'kampar'){
+            $fee = 2.0;
+        }
+
+        $this->pushSessionCart();
+        return $fee;
     }
 
 }

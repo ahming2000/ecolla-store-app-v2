bb6fb8d1a42985741e72c6df864550f6a60a3bcb
AhMing <keeshengming@outlook.com>
2021-04-03 17:11:57 +0800

    Cart page added

--- resources/views/ch/cart.blade.php
+++ resources/views/ch/cart.blade.php
@@ -1,6 +1,314 @@
 @extends('ch.layouts.customer')
 
 @section('content')
-    Test
+    <main class="container">
+        <div class="row">
+
+            <!-- Cart item and notification -->
+            <div class="col-lg-8">
+
+                <!-- Cart items -->
+                <div class="card mb-3">
+                    <div class="card-body" id="cartItemList">
+
+                        <div class="h4 pl-3 mb-3">购物车（{{ $cart->getCartCount() }}个）</div>
+
+                        @if($cart->getCartCount() == 0)
+                            <div class="text-center">
+                                <img src="{{asset('img/icon/empty-cart.png')}}" width="150" height="150"/>
+                                <div class="h5 p-2">您的购物车为空</div>
+                            </div>
+                        @endif
+
+                        @foreach($cart->cartItems as $cartItem)
+                            <div class="col-12 mb-3" id="{{ $cartItem->variation->barcode }}">
+                                <div class="row">
+
+                                    <!-- Cart Item Image -->
+                                    <div class="col-xs-12 col-sm-5 col-md-5 col-lg-5 col-xl-5">
+                                        <div class="view zoom z-depth-1 rounded mb-3">
+                                            <a href="/ch/item/{{ $cartItem->variation->item->name }}">
+                                                <img src="{{ asset($cartItem->variation->image ?? $cartItem->variation->item->getCoverImage()) }}" class="w-100" height="250">
+                                            </a>
+                                        </div>
+                                    </div><!-- Cart Item Image -->
+
+                                    <!-- Cart item information -->
+                                    <div class="col-xs-12 col-sm-7 col-md-7 col-lg-7 col-xl-7">
+
+                                        <!-- Item name display -->
+                                        <div class="h4 font-weight-bold">{{ $cartItem->variation->item->name }}</div>
+                                        <!-- Item name display -->
+
+                                        <!-- Variety property display -->
+                                        <div class="h6 grey-text">{{ $cartItem->variation->name1 . ' ' . $cartItem->variation->name2 }}</div>
+                                        <!-- Variety property display -->
+
+                                        <!-- Weight display -->
+                                        <div class="h6 text-muted">{{ number_format($cartItem->variation->weight * $cartItem->quantity, 3) . 'kg' }}</div>
+                                        <!-- Weight display -->
+
+                                        <!-- Quantity control -->
+                                        <div class="row align-items-center">
+                                            <div class="col-xs-12 col-sm-12 col-md-3">
+                                                数量：
+                                            </div>
+                                            <div class="col-xs-12 col-sm-12 col-md-9 d-flex justify-content-center">
+                                                <form action="/ch/cart" method="post">
+                                                    @csrf
+                                                    <input type="hidden" name="action" value="quantityAdjust">
+                                                    <input type="hidden" name="barcode" value="{{ $cartItem->variation->barcode }}">
+                                                    <input type="hidden" name="quantityToAdjust" value="-1">
+
+                                                    <button type="submit" class="btn btn-primary quantity-decrease-button" {{ $cartItem->quantity == 1 ? "disabled" : "" }}>-</button>
+                                                </form>
+                                                <input type="number" class="mx-3 my-3 cart-item-quantity" value="{{ $cartItem->quantity }}" style="width: 45px" disabled>
+                                                <form action="/ch/cart" method="post">
+                                                    @csrf
+                                                    <input type="hidden" name="action" value="quantityAdjust">
+                                                    <input type="hidden" name="barcode" value="{{ $cartItem->variation->barcode }}">
+                                                    <input type="hidden" name="quantityToAdjust" value="1">
+
+                                                    <button type="submit" class="btn btn-primary quantity-increase-button" {{ $cartItem->quantity == $cartItem->variation->getTotalStock() ? "disabled" : "" }}>+</button>
+                                                </form>
+                                            </div>
+                                        </div><!-- Quantity control -->
+
+                                        <!-- Remove button and price display -->
+                                        <div class="d-flex justify-content-between align-items-center">
+
+                                            <div>
+                                                <form action="/ch/cart" method="post">
+                                                    @csrf
+                                                    <input type="hidden" name="action" value="deleteItem">
+                                                    <input type="hidden" name="barcode" value="{{ $cartItem->variation->barcode }}">
+                                                    <button type="submit" class="btn btn-primary py-2 px-3 card-link-secondary small">
+                                                        <i class="fas fa-trash-alt mr-1"></i>移除
+                                                    </button>
+                                                </form>
+                                            </div>
+
+                                            @if(!$cartItem->variation->item->hasNoWholesale() && $cartItem->variation->discount == null)
+                                                @if($cartItem->variation->item->getWholesaleRate($cartItem->quantity))
+                                                    <div>
+                                                        <span class="grey-text mr-1" style="font-size: 15px"><del>RM{{ number_format($cartItem->variation->price * $cartItem->quantity, 2, '.', '') }}</del></span>
+                                                        <span>RM{{ number_format($cartItem->getSubPrice(), 2, '.', '') }}</span>
+                                                        <span class="badge badge-warning mr-1">{{ number_format((1 - $cartItem->variation->item->getWholesaleRate($cartItem->quantity)) * 100, 0, '.', '') }}% OFF</span>
+                                                    </div>
+                                                @else
+                                                    <div>
+                                                        <span>RM{{ number_format($cartItem->getSubPrice(), 2, '.', '') }}</span>
+                                                    </div>
+                                                @endif
+                                            @else
+                                                @if($cartItem->variation->discount != null)
+                                                    <div>
+                                                        <span class="grey-text mr-1" style="font-size: 15px"><del>RM<= number_format($cartItem->getItem()->getVarieties()[$cartItem->getVarietyIndex()]->getPrice() * $cartItem->getQuantity(), 2, '.', ''); ?></del></span>
+                                                        <span>RM{{ number_format($cartItem->getSubPrice(), 2, '.', '') }}</span>
+                                                        <span class="badge badge-danger mr-1">{{ number_format((1 - $cartItem->variation->discount->rate) * 100, 0, '.', '') }}% OFF</span>
+                                                    </div>
+                                                @else
+                                                    <div>
+                                                        <span>RM{{ number_format($cartItem->getSubPrice(), 2, '.', '') }}</span>
+                                                    </div>
+                                                @endif
+                                            @endif
+                                        </div><!-- Remove button and price -->
+
+                                    </div><!-- Cart item information price display-->
+
+                                </div>
+                            </div>
+
+                        @endforeach
+
+                        @if($cart->getCartCount() != 0)
+                            <div class="col-12">
+                                <form action="/ch/cart" method="post">
+                                    @csrf
+                                    <input type="hidden" name="action" value="resetCart">
+                                    <button class="btn btn-primary btn-block" type="submit">清空购物车</button>
+                                </form>
+                            </div>
+                        @endif
+
+                    </div>
+                </div><!-- Cart items -->
+
+                <!-- Notification -->
+                <div class="card mb-3">
+                    <div class="card-body bg-info">
+                        <!-- Delivery Description -->
+                        <h5>外送暂时只开放给金宝顾客</h5>
+                        <p class="text-light">
+                            外送价钱：RM2.00
+                        </p>
+                    </div>
+                </div><!-- Notification -->
+
+            </div><!-- Cart item and notification -->
+
+            <!-- Order mode settings and order summary -->
+            <div class="col-lg-4">
+
+                <!-- Order mode settings -->
+                <div class="card mb-3">
+                    <div class="card-body">
+                        <div class="h5 mb-3">订单模式：</div>
+                        <form action="/ch/cart" method="post">
+                            @csrf
+                            <input type="hidden" name="action" value="updateCartSettings">
+
+                            <select class="form-control mb-3 w-100" name="orderMode">
+                                <option value="pickup" {{ $cart->orderMode == 'pickup' ? " selected" : "" }}>预购取货</option>
+                                <option value="delivery" {{ $cart->orderMode == 'delivery' ? " selected" : "" }}>外送（仅限金宝区域）</option>
+                            </select>
+
+                            @if($cart->orderMode == 'delivery')
+
+                                <input type="hidden" name="customerField" value="enabled">
+
+                                <div class="form-group">
+                                    <label for="name">收货人名字（英语）</label>
+                                    <input type="text"
+                                           class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}"
+                                           name="name"
+                                           id="name"
+                                           value="{{ $cart->customer->name ?? old('name') ?? "" }}"
+                                           placeholder="请输入可辨识的姓名"/>
+                                    @if ($errors->has('name'))
+                                        <div class="invalid-feedback">
+                                            <strong>{{ $errors->first('name') }}</strong>
+                                        </div>
+                                    @endif
+                                </div>
+
+
+                                <div class="form-group">
+                                    <label for="phone">电话号码</label>
+                                    <input type="text"
+                                           class="form-control{{ $errors->has('phone') ? ' is-invalid' : '' }}"
+                                           name="phone"
+                                           id="phone"
+                                           value="{{ $cart->customer->phone ?? old('phone') ?? "" }}"
+                                           placeholder="电话号码">
+                                    @if ($errors->has('phone'))
+                                        <div class="invalid-feedback">
+                                            <strong>{{ $errors->first('phone') }}</strong>
+                                        </div>
+                                    @endif
+                                </div>
+
+
+                                <div class="form-group">
+                                    <label for="addressLine1">地址</label>
+                                    <input type="text"
+                                           name="addressLine1"
+                                           class="form-control{{ $errors->has('addressLine1') ? ' is-invalid' : '' }}"
+                                           value="{{ $cart->customer->addressLine1 ?? old('addressLine1') ?? "" }}"
+                                           placeholder="门牌/路名"/>
+                                    @if ($errors->has('addressLine1'))
+                                        <div class="invalid-feedback">
+                                            <strong>{{ $errors->first('addressLine1') }}</strong>
+                                        </div>
+                                    @endif
+                                </div>
+
+                                <div class="form-group">
+                                    <div class="form-row">
+                                        <div class="col">
+                                            <input type="text"
+                                                   name="state"
+                                                   class="form-control{{ $errors->has('state') ? ' is-invalid' : '' }}"
+                                                   value="{{ $cart->customer->state ?? old('state') ?? "" }}"
+                                                   placeholder="州属"/>
+                                            @if ($errors->has('state'))
+                                                <div class="invalid-feedback">
+                                                    <strong>{{ $errors->first('state') }}</strong>
+                                                </div>
+                                            @endif
+                                        </div>
+                                        <div class="col">
+                                            <input type="text" name="area"
+                                                   class="form-control{{ $errors->has('area') ? ' is-invalid' : '' }}"
+                                                   value="{{ $cart->customer->area ?? old('area') ?? "" }}"
+                                                   placeholder="地区/城市"/>
+                                            @if ($errors->has('area'))
+                                                <div class="invalid-feedback">
+                                                    <strong>{{ $errors->first('area') }}</strong>
+                                                </div>
+                                            @endif
+                                        </div>
+                                        <div class="col">
+                                            <input type="text" name="postal_code"
+                                                   class="form-control{{ $errors->has('postal_code') ? ' is-invalid' : '' }}"
+                                                   value="{{ $cart->customer->postal_code ?? old('postal_code') ?? "" }}"
+                                                   placeholder="邮政编号"/>
+
+                                            @if ($errors->has('postal_code'))
+                                                <div class="invalid-feedback">
+                                                    <strong>{{ $errors->first('postal_code') }}</strong>
+                                                </div>
+                                            @endif
+                                        </div>
+                                    </div>
+                                </div>
+
+                            @else
+
+                                <input type="hidden" name="orderVerifyIdField" value="enabled">
+
+                                <div class="form-group">
+                                    <label for="order_verify_id">领取代码（自行设置）</label>
+                                    <input type="text"
+                                           class="form-control{{ $errors->has('order_verify_id') ? ' is-invalid' : '' }}"
+                                           name="order_verify_id"
+                                           id="order_verify_id"
+                                           value="{{ $cart->orderVerifyId ?? old('order_verify_id') ?? "" }}"
+                                           placeholder="领取代码">
+                                    @if ($errors->has('order_verify_id'))
+                                        <div class="invalid-feedback">
+                                            <strong>{{ $errors->first('order_verify_id') }}</strong>
+                                        </div>
+                                    @endif
+                                </div>
+                            @endif
+
+                            <button class="btn btn-primary btn-block" type="submit">保存</button>
+                        </form>
+                    </div>
+                </div><!-- Order mode settings -->
+
+                <!-- Order summary -->
+                <div class="card mb-3">
+                    <div class="card-body">
+
+                        <h5 class="card-title">购物车订单摘要</h5>
+
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item d-flex justify-content-between align-items-center border-0 px-0">
+                                小计
+                                <span>RM {{ number_format($cart->getSubTotal(), 2) }}</span>
+                            </li>
+                            <li class="list-group-item d-flex justify-content-between align-items-center px-0">
+                                邮费
+                                <span>{{ $cart->orderMode == 'delivery' ? "RM " . number_format($cart->getShippingFee(), 2) : "N/A" }}</span>
+                            </li>
+                            <li class="list-group-item d-flex justify-content-between align-items-center border-0 px-0">
+                                <strong>总计</strong>
+                                <span><strong>RM {{ number_format($cart->getSubTotal() + $cart->getShippingFee(), 2) }}</strong></span>
+                            </li>
+                        </ul>
+
+                        <form action="/ch/check-out" method="get">
+                            <button class="btn btn-primary btn-block" type="submit" id="submit_btn" {{ $cart->canCheckOut ? "" : "disabled" }}>前往付款</button>
+                        </form>
+                    </div>
+                </div><!-- Order summary -->
+
+            </div><!-- Order mode settings and order summary -->
+
+        </div>
+    </main>
 
 @endsection

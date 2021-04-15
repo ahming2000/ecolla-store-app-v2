e441ee621b15f4c17b1980a0b2c059026b82ca38
AhMing <keeshengming@outlook.com>
2021-04-15 18:09:09 +0800

    Add order tracking function controller, add image storing function

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -8,6 +8,7 @@ use App\Models\OrderItem;
 use App\Session\Cart;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Intervention\Image\Facades\Image;
 
 class OrdersController extends Controller
 {
@@ -40,9 +41,14 @@ class OrdersController extends Controller
             'receipt_image' => ['required', 'image']
         ]);
 
+
+
         $prefix = DB::table('system_configs')->where('name', '=', 'orderCodePrefix')->value('value');
         $dateTime = date('Y-m-d H:i:s');
         $orderCode = $prefix . date_format(date_create($dateTime), "YmdHis");
+
+        $imagePath = request('receipt_image')->store('receipts', 'public');
+        Image::make(public_path("storage/$imagePath"))->save(); // Optimize image by default
 
         if($cart->orderMode == 'delivery'){
             $orderData = array_merge($orderData, [
@@ -56,6 +62,10 @@ class OrdersController extends Controller
                 'delivery_id' => $cart->deliveryId
             ]);
         }
+
+        // $orderData['receipt_image'] = "http://www.newrainbowmarket.com/storage/" . $imagePath;
+        $orderData['receipt_image'] = "http://ecolla.laravel/storage/" . $imagePath;
+
 
         $order = new Order();
         foreach ($orderData as $key => $value){
@@ -142,7 +152,61 @@ class OrdersController extends Controller
 
     public function orderTracking()
     {
-        return view($this->getLang() . '.order.order-tracking');
+        $message = "";
+
+        $code = request('code');
+
+        if($code != null){
+            $order = DB::table('orders')->whereIn('code', [$code])->first();
+            if($order != null){
+                if($order->mode == 'delivery'){
+                    switch ($order->status){
+                        case 'pending':
+                            $message = $this->getLang() == 'ch' ? "订单正在处理中" : "Processing Order";
+                            break;
+                        case 'prepared':
+                            $message = $this->getLang() == 'ch' ? "订单已出货" : "Order is delivered";
+                            break;
+                        case 'completed':
+                            $message = $this->getLang() == 'ch' ? "订单已完成" : "Order is completed";
+                            break;
+                        case 'refunded':
+                            $message = $this->getLang() == 'ch' ? "订单已退款" : "Order is refunded";
+                            break;
+                        case 'canceled':
+                            $message = $this->getLang() == 'ch' ? "订单已取消" : "Order is cancelled";
+                            break;
+                        default:
+                            $message = "Null";
+                    }
+                } else{
+                    switch ($order->status){
+                        case 'pending':
+                            $message = $this->getLang() == 'ch' ? "订单正在准备中" : "Preparing Order";
+                            break;
+                        case 'prepared':
+                            $message = $this->getLang() == 'ch' ? "订单已准备完毕，请到店里取货" : "Order is well prepared, please pick up at the store.";
+                            break;
+                        case 'completed':
+                            $message = $this->getLang() == 'ch' ? "订单已完成" : "Order is completed";
+                            break;
+                        case 'refunded':
+                            $message = $this->getLang() == 'ch' ? "订单已退款" : "Order is refunded";
+                            break;
+                        case 'canceled':
+                            $message = $this->getLang() == 'ch' ? "订单已取消" : "Order is cancelled";
+                            break;
+                        default:
+                            $message = 'Null';
+                    }
+                }
+            } else{
+                $message = $this->getLang() == 'ch' ? "订单不存在" : "Order not found";
+            }
+
+        }
+
+        return view($this->getLang() . '.order.order-tracking', compact('message'));
     }
 
 }

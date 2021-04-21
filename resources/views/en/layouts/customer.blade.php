42d56bf1c0275a1f14eda67dc92504d9a09dce9b
AhMing <keeshengming@outlook.com>
2021-04-21 22:36:03 +0800

    Change the icofont to min version

--- resources/views/en/layouts/customer.blade.php
+++ resources/views/en/layouts/customer.blade.php
@@ -41,7 +41,7 @@ $cart->start();
     <!-- Styles -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/css/mdb.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('vendor/icofont/icofont.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/icofont/icofont.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/global.css') }}">
 
     <style>

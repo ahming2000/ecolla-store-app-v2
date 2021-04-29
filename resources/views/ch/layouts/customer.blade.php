7ce7ce3c35337a42a6a5f3baedf180092e5b7a6a
AhMing <keeshengming@outlook.com>
2021-04-30 01:22:04 +0800

    Add min 200px responsive css This help to display 2 div a row in mobile view

--- resources/views/ch/layouts/customer.blade.php
+++ resources/views/ch/layouts/customer.blade.php
@@ -38,10 +38,10 @@ $cart->start();
     <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>
 
     <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('css/global.css') }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/css/mdb.min.css" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('vendor/icofont/icofont.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/global.css') }}">
 
     <style>
         body {

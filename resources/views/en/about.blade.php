9cdc80e957b08c550c8e87d3ca0a74f143b41c0d
AhMing <keeshengming@outlook.com>
2021-05-13 23:01:58 +0800

    About page view update

--- resources/views/en/about.blade.php
+++ resources/views/en/about.blade.php
@@ -4,35 +4,37 @@
     About Us | Ecolla
 @endsection
 
+@section('extraStyle')
+    <style>
+        .about,.us{
+            display: inline;
+        }
+        .us{
+            color: hotpink;
+        }
+    </style>
+@endsection
+
 @section('content')
-    <main class="flex-fill"> <!--put content-->
-        <section class="mt-5">
-            <div class="container">
-                <div class="row pl-3 mx-auto">
-                    <div class="col pt-5">
-                        <h1 class="about">About</h1><h1 class="us"> Us</h1>
-                        <p class="mt-4">Snacks from foreign country China Thailand Korea<br>Best taste ever</p>
-                    </div>
-                    <div class="col">
-                        <img src="{{ asset('img/ads/shop-image.jpg') }}" height="350" width="410" alt="image">
-                    </div>
-                </div>
+    <main class="container">
+        <div class="row pl-3 mx-auto mb-5">
+            <div class="col pt-5">
+                <h1 class="about">About</h1><h1 class="us"> Us</h1>
+                <p class="mt-4">Snacks from foreign country China Thailand Korea<br>Best taste ever</p>
             </div>
-        </section>
-
-        <section class="mt-5">
-            <div class="container">
-                <div class="row mx-auto">
-                    <div class="col">
-                        <img src="{{ asset('img/ads/shop-image.jpg') }}" height="350" width="410" alt="image">
-                    </div>
-                    <div class="col pt-5">
-                        <h1>Operation Hours</h1>
-                        <p class="mt-4">10AM - 10PM</p>
-                    </div>
-                </div>
+            <div class="col">
+                <img src="{{ asset('img/ads/shop-image.jpg') }}" height="350" width="410" alt="image">
             </div>
-        </section>
+        </div>
 
+        <div class="row mx-auto">
+            <div class="col">
+                <img src="{{ asset('img/ads/shop-image.jpg') }}" height="350" width="410" alt="image">
+            </div>
+            <div class="col pt-5">
+                <h1>Operation Hours</h1>
+                <p class="mt-4">10AM - 10PM</p>
+            </div>
+        </div>
     </main>
 @endsection

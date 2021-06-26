663fc6e7e6a28a3b2cd90dfe5a45a28588daa57c
AhMing <keeshengming@outlook.com>
2021-06-27 01:24:27 +0800

    Update item index page

--- resources/views/en/item/index.blade.php
+++ resources/views/en/item/index.blade.php
@@ -53,14 +53,14 @@
             <div class="row mb-3">
 
 
-
-            <!-- Search -->
+                <!-- Search -->
                 <div class="col-sm-12 col-md-6">
                     <form action="{{ url('/en/item') }}" method="get">
                         <div class="d-flex justify-content-between">
                             <div class="flex-grow-1 me-2">
                                 <input type="text" class="form-control shadow" maxlength="20" name="search"
-                                       value="{{ $_GET["search"] ?? "" }}" placeholder="Search name, barcode, variation, origin, item desc">
+                                       value="{{ $_GET["search"] ?? "" }}"
+                                       placeholder="Search name, barcode, variation, origin, item desc">
                             </div>
                             <div>
                                 <button type="submit" class="btn btn-primary shadow">
@@ -96,53 +96,56 @@
             <div class="row mb-3">
                 @foreach($items as $item)
                     <div class="col-6 col-md-4 col-lg-3 col-xxl-2 mb-3">
-                        <div class="card shadow" onclick="window.location.href = '{{ url('/en/item/' . $item->id) }}'">
+                        <a href="{{ url('/en/item/' . $item->id) }}" class="no-anchor-style">
+                            <div class="card shadow">
 
-                            {{-- Image --}}
-                            @if(!empty($item->images->toArray()))
-                                <a href="{{ url('/en/item/' . $item->id) }}">
-                                    <img src="{{ $item->getCoverImage() }}" class="card-img-top" alt="image"
-                                         loading="lazy">
-                                </a>
-                            @endif
-                            {{-- Image --}}
-
-                            <div class="card-body">
-
-                                {{-- Title --}}
-                                <div class="h5 card-title text-truncate">
-                                    {{ $item->name_en }}
-                                </div>
-                                {{-- Title --}}
-
-                                {{-- Price --}}
-                                <span class="price-text-normal">
-                                    @if($item->getPriceRange()['min'] == $item->getPriceRange()['max'])
-                                        RM{{ $item->getPriceRange()['min'] }}
-                                    @else
-                                        RM{{ $item->getPriceRange()['min'] }} - RM{{ $item->getPriceRange()['max'] }}
-                                    @endif
-                                </span>
-                                {{-- Price --}}
-
-                                {{-- Wholesale Badge --}}
-                                @if(!empty($item->discounts->toArray()))
-                                    <span class="badge rounded-pill bg-info">Wholesale</span>
+                                {{-- Image --}}
+                                @if(!empty($item->images->toArray()))
+                                    <a href="{{ url('/en/item/' . $item->id) }}">
+                                        <img src="{{ $item->getCoverImage() }}" class="card-img-top" alt="image"
+                                             loading="lazy">
+                                    </a>
                                 @endif
-                                {{-- Wholesale Badge --}}
+                                {{-- Image --}}
 
-                                {{-- Stock & Total View --}}
-                                <div class="d-flex justify-content-between">
-                                    <div>
-                                        <i class="icofont icofont-box"></i> {{ $item->getTotalStock() }}
+                                <div class="card-body">
+
+                                    {{-- Title --}}
+                                    <div class="h5 card-title text-truncate">
+                                        {{ $item->name_en }}
                                     </div>
-                                    <div>
-                                        <i class="icofont icofont-eye"></i> {{ $item->util->view_count }}
+                                    {{-- Title --}}
+
+                                    {{-- Price --}}
+                                    <span class="price-text-normal">
+                                    @if($item->getPriceRange()['min'] == $item->getPriceRange()['max'])
+                                            RM{{ $item->getPriceRange()['min'] }}
+                                        @else
+                                            RM{{ $item->getPriceRange()['min'] }} -
+                                            RM{{ $item->getPriceRange()['max'] }}
+                                        @endif
+                                </span>
+                                    {{-- Price --}}
+
+                                    {{-- Wholesale Badge --}}
+                                    @if(!empty($item->discounts->toArray()))
+                                        <span class="badge rounded-pill bg-info">Wholesale</span>
+                                    @endif
+                                    {{-- Wholesale Badge --}}
+
+                                    {{-- Stock & Total View --}}
+                                    <div class="d-flex justify-content-between">
+                                        <div>
+                                            <i class="icofont icofont-box"></i> {{ $item->getTotalStock() }}
+                                        </div>
+                                        <div>
+                                            <i class="icofont icofont-eye"></i> {{ $item->util->view_count }}
+                                        </div>
                                     </div>
+                                    {{-- Stock & Total View --}}
                                 </div>
-                                {{-- Stock & Total View --}}
                             </div>
-                        </div>
+                        </a>
                     </div>
                 @endforeach
             </div>
